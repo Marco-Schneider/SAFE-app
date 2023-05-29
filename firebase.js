f6a4469c4bcad19c29ae07750675f62cd74362cb
Marco-Schneider <marco.schneider452@gmail.com>
2023-05-28 23:52:47 -0300

    Refactoring to adjust to new firebase version

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import *as firebase from "firebase/app";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,13 +16,12 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
 if(firebase.apps.length === 0) {
-  const app = initializeApp(firebaseConfig);
+  let app = firebase.initializeApp(firebaseConfig);
 } else {
   app = firebase.app();
 }
 
-const auth =  firebase.auth();
+const auth = firebase.auth();
 
 export { auth };
