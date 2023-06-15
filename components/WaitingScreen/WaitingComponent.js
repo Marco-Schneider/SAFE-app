6abfd9c5f39dcfe5cd844ec728a83a66b1c84bec
Marco-Schneider <marco.schneider452@gmail.com>
2023-06-14 23:11:22 -0300

    Initial implementation for the communication between esp32 and the app

--- components/WaitingScreen/WaitingComponent.js
+++ components/WaitingScreen/WaitingComponent.js
@@ -13,10 +13,12 @@ function WaitingScreen({navigation}) {
 
   const route = useRoute();
   const { selectedItems } = route.params;
+  const indicator = false;
 
   useEffect(() => {
     console.log("THE USER SELECTED THE FOLLOWING ITEMS: ", selectedItems);
     handleUpdateAvailability();
+    handleOpeningOfCabinet();
   }, [])
 
   const handleUpdateAvailability = async () => {
@@ -28,8 +30,11 @@ function WaitingScreen({navigation}) {
         }
       }));
 
+      console.log("UpdatedItems ",updatedItems)
+
       const updateRequests = updatedItems.map((item) => {
         const documentName = `projects/safe-auth-a2ae0/databases/(default)/documents/tools/${item.name}`;
+        console.log("mapping", item);
         return fetch(`https://firestore.googleapis.com/v1/${documentName}`, {
           method: 'PATCH',
           headers: {
@@ -56,11 +61,60 @@ function WaitingScreen({navigation}) {
       console.log('Error updating items:', error);
     }
   };
+
+  const handleOpeningOfCabinet = async () => {
+
+    const doorsToBeOpened = {
+      "objects": []
+    }
+
+    doorsToBeOpened["objects"] = selectedItems.map((item) => ({
+      "door": item.location.integerValue,
+      "state": 0
+    }))
+
+    console.log("Doors to be opened: ", doorsToBeOpened)
+
+    try {      
+      const response = await fetch('http://192.168.0.12/control/retrieve', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(doorsToBeOpened)
+      });
   
+      if (response.ok) {
+        const responseData = await response.text();
+        console.log('Response:', responseData);
+      } else {
+        const errorBody = await response.text();
+        console.log('Error:', response.status);
+        console.log('Error body:', errorBody);
+      }
+    } 
+    catch (error) {
+      console.log('Error 2:', error.message);
+    }
+  };
 
   return (
     <View style={styles.appContainer}>
-      <View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Por favor, retire os itens confome indicado painel do SAFE</Text>
+        { indicator && (
+          <View>
+            <Text style={styles.text}>Gostaria de retirar mais items?</Text>
+            <View style={styles.buttonsContainer}>
+              <Button 
+                title='Sim'
+              />
+              <Button 
+                title='Não'
+              />
+            </View>
+          </View>
+        )}
       </View>
     </View>
   )
@@ -68,15 +122,25 @@ function WaitingScreen({navigation}) {
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex: 1, 
-    backgroundColor: '#0C479D'
+    flex: 1,
+    backgroundColor: '#0C479D',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   textContainer: {
-    fontSize: 25,
     marginTop: 25,
-    textAlign: 'center',
     width: 350,
-  }
+  },
+  text: {
+    fontSize: 25,
+    textAlign: 'center',
+    color: 'white',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 10,
+  },
 })
 
 export default WaitingScreen;
